026bbe0e3a2a3e1fe6a22acebcc705c118f9b111
zhongxuan_li <aa234258202@qq.com>
2023-03-05 18:11:19 +0800

    func get put

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,1 +1,13 @@
 package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"kos/pkg/service"
+)
+
+func RegisterApi(addr string) {
+	router := gin.Default()
+	router.GET("/objects/get", service.GetObject)
+	router.PUT("/objects/put", service.PutObject)
+	router.Run(addr)
+}
--- pkg/service/object.go
+++ pkg/service/object.go
@@ -1,1 +1,52 @@
 package service
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+import "github.com/gin-gonic/gin"
+
+func GetObject(c *gin.Context) {
+	f, err := os.Create(os.Getenv("STORAGE_ROOT") + "/objects/" + strings.Split(c.Request.URL.EscapedPath(), "/")[2])
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "not find",
+		})
+		return
+	}
+	defer f.Close()
+	_, err = io.Copy(c.Writer, f)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "bad copy",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+	})
+	return
+}
+func PutObject(c *gin.Context) {
+	f, err := os.Create(os.Getenv("STORAGE_ROOT") + "/objects/" + strings.Split(c.Request.URL.EscapedPath(), "/")[2])
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "not find",
+		})
+		return
+	}
+	defer f.Close()
+	_, err = io.Copy(f, c.Request.Body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "bad copy",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+	})
+	return
+}
